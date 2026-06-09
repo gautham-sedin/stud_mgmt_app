1404e344919f4e6ade7c6a55944b71e9142b689d
gautham-sedin <gautham.r@sedintechnologies.com>
2026-06-09 11:13:41 +0000

    feat: create student model with validations and result logic

---
+++ app/models/student.rb
@@ -0,0 +1,32 @@
+class Student < ApplicationRecord
+  validates :name, presence: true
+
+  validates :email, 
+    presence: true,
+    uniqueness: true
+
+  validates :age,
+    presence: true,
+    numericality: {
+      greater_than: 0
+    }
+
+  validates :course, 
+    presence: true
+
+  validates :city,
+    presence: true
+
+  validates :marks,
+    numericality: {
+      greater_than_or_equal_to: 0,
+      less_than_or_equal_to: 100
+    },
+    allow_nil: true
+
+  def result
+    return "NA" if marks.nil?
+
+    marks >= 35 ? "Pass" : "Fail"
+  end
+end
---
+++ db/migrate/20260609110105_create_students.rb
@@ -0,0 +1,16 @@
+class CreateStudents < ActiveRecord::Migration[8.0]
+  def change
+    create_table :students do |t|
+      t.string :name
+      t.string :email
+      t.integer :age
+      t.string :course
+      t.string :city
+      t.integer :marks
+
+      t.timestamps
+    end
+
+    add_index :students, :email, unique: true
+  end
+end
