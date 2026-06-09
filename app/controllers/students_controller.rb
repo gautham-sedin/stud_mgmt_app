27892fb371f858987cf85a2227d543a36db5cf23
gautham-sedin <gautham.r@sedintechnologies.com>
2026-06-09 11:42:45 +0000

    feat: implement students controller with RESTful actions

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,61 @@
+class StudentsController < ApplicationController
+  before_action :set_student, only: [
+    :show,
+    :edit,
+    :update,
+    :destroy
+  ]
+
+  def index
+    @students = Student.all
+  end
+
+  def show
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new(student_params)
+
+    if @student.save
+      redirect_to @student, notice: "Student created successfully."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @student.update(student_params)
+      redirect_to @student, notice: "Student updated successfully."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @student.destroy
+
+    redirect_to students_path, notice: "Student deleted successfully."
+  end
+
+  def set_student
+    @student = Student.find(params[:id])
+  end
+
+  def student_params
+    params.require(:student).permit(
+      :name,
+      :email,
+      :age,
+      :course,
+      :city,
+      :marks
+    )
+  end
+end
