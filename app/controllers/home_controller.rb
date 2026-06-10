fa0d36563a63db95ad46985161f57ab0f5b7e7a3
gautham-sedin <gautham.r@sedintechnologies.com>
2026-06-10 05:48:44 +0000

    style: redesigned the dashboard ui & its implementations

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,17 @@
 class HomeController < ApplicationController
   def index
+
+    @total_students = Student.count
+
+    @java_students = Student.where(course: "Java").count
+
+    @react_students = Student.where(course: "React").count
+
+    @ruby_students = Student.where(course: "Ruby").count
+
+    @rails_students = Student.where(course: "Rails").count
+
+    @recent_students = Student.order(created_at: :desc).limit(5)
+
   end
 end
