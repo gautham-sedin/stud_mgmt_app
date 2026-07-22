6c26c503a212b8a39827bfeb10c2d96cc63e6f87
gautham-sedin <gautham.r@sedintechnologies.com>
2026-07-22 06:11:46 +0000

    fix: Providing basic auth credential for sidekiq for web ui #42

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,22 @@
 require "sidekiq/web"
 
 Rails.application.routes.draw do
-  authenticate :user, lambda { |user| user.admin? } do
-    mount Sidekiq::Web => "/sidekiq"
+  if Rails.env.production?
+    Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+
+      ActiveSupport::SecurityUtils.secure_compare(
+        username,
+        ENV["SIDEKIQ_USERNAME"]
+      ) &
+
+      ActiveSupport::SecurityUtils.secure_compare(
+        password,
+        ENV["SIDEKIQ_PASSWORD"]
+      )
+    end
   end
+
+  mount Sidekiq::Web => "/sidekiq"
 
   devise_for :users
   root "home#index"
